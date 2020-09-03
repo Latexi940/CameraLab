a41d941522e5e8d380505e6c1a03b5a58889c74d
laurinis <lauri.nissinen@metropolia.fi>
2020-09-03 11:26:29 +0300

    Images are now saved on device storage

--- app/src/main/java/com/example/cameralab/MainActivity.kt
+++ app/src/main/java/com/example/cameralab/MainActivity.kt
@@ -2,26 +2,50 @@ package com.example.cameralab
 
 import android.app.Activity
 import android.content.Intent
-import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
 import android.provider.MediaStore
 import android.util.Log
+import androidx.core.content.FileProvider
 import kotlinx.android.synthetic.main.activity_main.*
+import java.io.File
 
 class MainActivity : AppCompatActivity() {
 
-    private val REQUEST_IMAGE_CAPTURE = 99
+    private val REQUEST_IMAGE_CAPTURE = 1
+    private var imageFile: File? = null
+    private var mCurrentPhotoPath: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        Log.i("CAMLAB", "Starting app")
+
+        val fileName = "temp_photo"
+        val imgPath = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+
+        imageFile = File.createTempFile(fileName, ".jpg", imgPath)
+
+        mCurrentPhotoPath = imageFile!!.absolutePath
+
+        Log.i("CAMLAB", "Photos will be saved in $mCurrentPhotoPath")
+
+        val photoURI: Uri = FileProvider.getUriForFile(
+            this,
+            "com.example.cameralab.fileprovider",
+            imageFile!!
+        )
+
         cameraButton.setOnClickListener {
             Log.i("CAMLAB", "Camera button pressed")
             val myIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-            if (myIntent.resolveActivity(packageManager) != null)
-            {
+            if (myIntent.resolveActivity(packageManager) != null) {
+                myIntent.flags = Intent.FLAG_GRANT_WRITE_URI_PERMISSION;
+                myIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
                 startActivityForResult(myIntent, REQUEST_IMAGE_CAPTURE)
             }
         }
@@ -31,8 +55,7 @@ class MainActivity : AppCompatActivity() {
         super.onActivityResult(requestCode, resultCode, recIntent)
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == Activity.RESULT_OK) {
             Log.i("CAMLAB", "Picture is good")
-            val extras = recIntent!!.extras
-            val imageBitmap = extras!!.get("data") as Bitmap
+            val imageBitmap = BitmapFactory.decodeFile(mCurrentPhotoPath)
             imageView.setImageBitmap(imageBitmap)
         }
     }
